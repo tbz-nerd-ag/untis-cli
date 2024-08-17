eba80cf648423c3900df1a2213e42ccc43df6210
ingressy <j.czinzoll@ingressy.de>
2024-08-17 18:00:15 +0200

    a little idea

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ s = webuntis.Session(
 
 s.login()
 
-print("TBZ Mitte Untis CLI v0.3 ||Table Update||\nby ingressy\n")
+print("TBZ Mitte Untis CLI v0.4\nby ingressy\n")
 
 
 while True:
@@ -116,8 +116,40 @@ while True:
 
                        table.add_row([s,e, k, t, r, sub])
                 print(table)
+        elif cli == "doorsign":
+                choose = input("Von welchen Raum möchtest du den Stundenplan haben? ")
+                choose1 = input("An welchen Tag den? ")
+                choose2 = input("Um welche Uhrzeit? ")
 
+                start = datetime.datetime.now()
+                end = start + datetime.timedelta(days=5)
 
+                rooms = s.rooms().filter(name=choose)
+
+                tt = s.timetable(room=rooms[0], start=start, end=end)
+                tt = sorted(tt, key=lambda x: x.start)
+                # print(tt)
+
+                time_format_end = "%H:%M"
+                time_format_start = "%Y-%m-%d %a " + time_format_end
+
+                #table = PrettyTable()
+                #table.field_names = ["Anfang", "Ende", "Klasse", "Lehrer", "Raum", "Fach"]
+
+                for po in tt:
+                        s = po.start.strftime(time_format_start)
+                        e = po.end.strftime(time_format_end)
+                        k = " ".join([k.name for k in po.klassen])
+                        t = " ".join([t.name for t in po.teachers])
+                        r = " ".join([r.name for r in po.rooms])
+                        sub = " ".join([r.name for r in po.subjects])
+                        c = "(" + po.code + ")" if po.code is not None else ""
+                        # print(s + "-" + e, k, sub, t, r, c)
+                        print (e)
+                        #if e > choose1:
+                                #print(s + "-" + e, k, sub, t, r, c)
+                        #table.add_row([s, e, k, t, r, sub])
+                #print(table)
         elif cli == "exit":
                 s.logout()
                 break
