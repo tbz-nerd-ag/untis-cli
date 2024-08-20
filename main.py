502b12bfee82bf4c26647d67c0e83a0392b81776
ingressy <j.czinzoll@ingressy.de>
2024-08-20 20:49:13 +0200

    a little update ^^

--- main.py
+++ main.py
@@ -24,46 +24,53 @@ s.login()
 print("TBZ Mitte Untis CLI v0.4 \nby ingressy\n")
 ramtext = ["thanks to Monster Energy and Katjes for mental support", "buy me a coffe pls ... i hate this shit", "READY.", "you are hacked", "My unicorn says: reality lies", "i like pancakes"]
 print(random.choice(ramtext))
+
 while True:
         cli = input("ingressy@untis ~$ ")
-
-        if cli == "allclasses":
-                for klasse in s.klassen():
-                        print(klasse.name)
-        elif cli == "allrooms":
-                for raum in s.rooms():
-                        print(raum.name)
-        elif cli == "allstudents":
-                for schueler in s.students():
-                        print(schueler.id)
-        elif cli == "allteachers":
-                choose = input("Kuerzel, Voller Name oder eine Liste\n"
-                                "k | v | l ~$ ")
-                if choose == "v":
-                        for lehrer in s.teachers():
-                                print(lehrer.full_name)
-                elif choose == "k":
-                        for lehrer in s.teachers():
-                                print(lehrer.name)
-                elif choose == "l":
-                        if STS == "dev":
-                                table = PrettyTable
+        if cli == "all":
+                acli = input("ingressy@untis /all/ ~$ ")
+                if acli == "classes":
+                        for klasse in s.klassen():
+                                print(klasse.name)
+                elif acli == "rooms":
+                        for raum in s.rooms():
+                                print(raum.name)
+                elif acli == "students":
+                        for schueler in s.students():
+                                print(schueler.full_name)
+                elif acli == "subjects":
+                        for feacher in s.subjects():
+                                print(feacher.long_name)
+                elif acli == "teachers":
+                        choose = input("Kuerzel, Voller Name oder eine Liste\n"
+                                       "k | v | l ~$ ")
+                        if choose == "v":
                                 for lehrer in s.teachers():
-                                        table.add_column([0], "Kürzel",[lehrer.name])
-                                        table.add_column([1],"Voller Name",[lehrer.full_name])
+                                        print(lehrer.full_name)
+                        elif choose == "k":
+                                for lehrer in s.teachers():
+                                        print(lehrer.name)
+                        elif choose == "l":
+                                table = PrettyTable()
+                                table.field_names = ["Kürzel", "Voller Namen"]
+
+                                for lehrer in s.teachers():
+                                        ln = lehrer.name
+                                        lf = lehrer.full_name
+
+                                        print(lehrer.name)
+                                        print(lehrer.full_name)
                         else:
-                                print("Diese Funktion ist noch in der Entwichklung!")
-                else:
-                        False
+                                False
+
         elif cli == "timetable":
                 choose = input("Von welcher Klasse möchtest du den Stundenplan haben? ")
 
-                today = datetime.date.today()
-                monday = today - datetime.timedelta(days=today.weekday())
-                friday = monday + datetime.timedelta(days=4)
+                start = datetime.datetime.now()
+                end = start + datetime.timedelta(days=5)
 
                 klasse = s.klassen().filter(name=choose)
-                tt = s.timetable(klasse=klasse[0], start=monday, end=friday)
+                tt = s.timetable(klasse=klasse[0], start=start, end=end)
                 tt = sorted(tt, key=lambda x: x.start)
                 print(tt)
 
@@ -71,19 +78,19 @@ while True:
                 time_format_start = "%Y-%m-%d %a " + time_format_end
 
                 table = PrettyTable()
-                table.field_names = ["Anfang", "Ende", "Klasse", "Lehrer", "Raum", "Fach"]
+                table.field_names = ["Anfang", "Ende", "Lehrer", "Raum", "Fach"]
 
                 for po in tt:
                         s = po.start.strftime(time_format_start)
                         e = po.end.strftime(time_format_end)
                         k = " ".join([k.name for k in po.klassen])
-                        #t = " ".join([t.name for t in po.teachers])
+                        t = " ".join([t.name for t in po.teachers])
                         r = " ".join([r.name for r in po.rooms])
                         sub = " ".join([r.name for r in po.subjects])
                         c = "(" + po.code + ")" if po.code is not None else ""
                         #print(s + "-" + e, k, sub, t, r, c)
 
-                        table.add_row([s, e, k, t, r, sub])
+                        table.add_row([s, e, t, r, sub])
                 print(table)
 
         elif cli == "rooms":
@@ -118,7 +125,7 @@ while True:
                 print(table)
         elif cli == "doorsign":
                 choose = input("Von welchen Raum möchtest du den Stundenplan haben? ")
-                chtime = input("Wie spät ist es?")
+                chtime = input("Wie spät ist es? ")
 
                 start = datetime.datetime.now()
                 end = start + datetime.timedelta(days=5)
